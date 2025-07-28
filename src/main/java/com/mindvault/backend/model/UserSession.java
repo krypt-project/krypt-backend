143365446fd7e3f9a208c84629173bccf3c12348
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-07-28 23:15:15 +0200

    feat: create UserSession

---
+++ src/main/java/com/mindvault/backend/model/UserSession.java
@@ -0,0 +1,35 @@
+package com.mindvault.backend.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "userSession")
+public class UserSession {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime expirationData;
+
+    public UserSession() {}
+
+    public UserSession(User user, String token, LocalDateTime expirationData) {
+        this.user = user;
+        this.token = token;
+        this.expirationData = expirationData;
+    }
+
+    public boolean isExpired() {
+        return LocalDateTime.now().isAfter(expirationData);
+    }
+}
