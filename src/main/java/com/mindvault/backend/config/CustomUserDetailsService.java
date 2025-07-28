961a2cec9cec32b9d16c687bac5463ff6edfe02b
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-07-28 22:07:58 +0200

    feat: create CustomUserDetailsService

---
+++ src/main/java/com/mindvault/backend/config/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.mindvault.backend.config;
+
+import com.mindvault.backend.model.User;
+import com.mindvault.backend.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmailUser(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .build();
+    }
+}
