37d706c4e0bb019fb17c2f9f8f2223c90e75c6d3
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-07-28 23:16:02 +0200

    feat: create EmailService

---
+++ src/main/java/com/mindvault/backend/service/EmailService.java
@@ -0,0 +1,30 @@
+package com.mindvault.backend.service;
+
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(String to, String subject, String body) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+
+            mailSender.send(message);
+        } catch (Exception e) {
+            throw new RuntimeException("Error while sending mail : ", e);
+        }
+    }
+}
