c54a59378f852e1cad33d2dd19bed96a3a4754c7
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-07-29 15:37:39 +0200

    refactor: add Setter and Constructor to AuthenticationDTO

--- src/main/java/com/mindvault/backend/dto/UserDTO/AuthenticationDTO.java
+++ src/main/java/com/mindvault/backend/dto/UserDTO/AuthenticationDTO.java
@@ -1,20 +1,41 @@
-package com.mindvault.backend.dto;
+package com.mindvault.backend.dto.UserDTO;
 
 public class AuthenticationDTO {
     private String email;
     private String password;
     private String token;
 
-    // Getter
+    public AuthenticationDTO() {}
+
+    public AuthenticationDTO(String email, String password, String token) {
+        this.email = email;
+        this.password = password;
+        this.token = token;
+    }
+
+    // Getter & Setter
+
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getToken() {
         return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 }
