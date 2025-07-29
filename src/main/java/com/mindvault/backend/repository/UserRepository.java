21143547a10f3451ce297a6f95e9866fac94ec4e
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-07-29 15:43:19 +0200

    feat: update UserRepository method name

--- src/main/java/com/mindvault/backend/repository/UserRepository.java
+++ src/main/java/com/mindvault/backend/repository/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmailUser(String emailUser);
+    Optional<User> findByEmail(String emailUser);
 }
