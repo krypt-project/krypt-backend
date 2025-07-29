a1141ff5009b68ec307d2299c5e56395d2a00c21
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-07-29 16:29:15 +0200

    feat: add findAllByUser method to TokenRepository

--- src/main/java/com/mindvault/backend/repository/TokenRepository.java
+++ src/main/java/com/mindvault/backend/repository/TokenRepository.java
@@ -1,10 +1,13 @@
 package com.mindvault.backend.repository;
 
 import com.mindvault.backend.model.Token;
+import com.mindvault.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
     Optional<Token> findByToken(String token);
+    List<Token> findAllByUser(User user);
 }
