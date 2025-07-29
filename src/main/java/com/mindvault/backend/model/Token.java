4c190db830c942272d20fe13270c4dba8e10cfc4
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-07-29 15:22:13 +0200

    feat: create Token model

---
+++ src/main/java/com/mindvault/backend/model/Token.java
@@ -0,0 +1,93 @@
+package com.mindvault.backend.model;
+
+import com.mindvault.backend.model.enums.TokenType;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "token")
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "token_id")
+    private Long id;
+
+    @Column(unique = true)
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TokenType tokenType = TokenType.ACCESS;
+
+    @Column(nullable = false)
+    private boolean expired;
+
+    @Column(nullable = false)
+    private boolean revoked;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // Constructor
+
+    public Token() {}
+
+    public Token(Long id, String token, TokenType tokenType, boolean expired, boolean revoked, User user) {
+        this.id = id;
+        this.token = token;
+        this.tokenType = tokenType;
+        this.expired = expired;
+        this.revoked = revoked;
+        this.user = user;
+    }
+
+    // Getter & Setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public TokenType getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(TokenType tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public boolean isExpired() {
+        return expired;
+    }
+
+    public void setExpired(boolean expired) {
+        this.expired = expired;
+    }
+
+    public boolean isRevoked() {
+        return revoked;
+    }
+
+    public void setRevoked(boolean revoked) {
+        this.revoked = revoked;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
