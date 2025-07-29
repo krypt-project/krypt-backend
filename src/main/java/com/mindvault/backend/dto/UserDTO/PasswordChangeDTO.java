88e3981b2244fde681b3801a72b591034798a19a
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-07-29 16:28:53 +0200

    feat: create PasswordChangeDTO

---
+++ src/main/java/com/mindvault/backend/dto/UserDTO/PasswordChangeDTO.java
@@ -0,0 +1,31 @@
+package com.mindvault.backend.dto.UserDTO;
+
+public class PasswordChangeDTO {
+    private String oldPassword;
+    private String newPassword;
+
+    public PasswordChangeDTO() {}
+
+    public PasswordChangeDTO(String oldPassword, String newPassword) {
+        this.oldPassword = oldPassword;
+        this.newPassword = newPassword;
+    }
+
+    // Getter & Setter
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
