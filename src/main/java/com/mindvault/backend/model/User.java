75d62cecc37115770247b6f1f1b298cc91c3ac5f
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-07-31 12:50:38 +0200

    refactor: update User Model database name

--- src/main/java/com/mindvault/backend/model/User.java
+++ src/main/java/com/mindvault/backend/model/User.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "user")
+@Table(name = "user_table")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
