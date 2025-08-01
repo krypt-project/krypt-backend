6892d2a3dd2b5bb5b4c970ea72ab6e2b6a8aeaa5
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-08-01 14:13:25 +0200

    build: add test data

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -1,2 +1,6 @@
 -- Database Initialisation
-SELECT 1;
+-- User
+INSERT INTO user_table(last_name, first_name, email, password, creation_date, modification_date, email_verified) VALUES
+('Durand', 'Alice', 'alice.durand@example.com', '$2y$10$eqjReaDGnKGfJbGHGib7te.mCZoaLDmcby8GYw1jlIpVoLZykM4kq', '2025-08-01T10:00:00', '2025-08-01T10:00:00', true),
+('Martin', 'Bob', 'bob.martin@example.com', '$2y$10$eqjReaDGnKGfJbGHGib7te.mCZoaLDmcby8GYw1jlIpVoLZykM4kq', '2025-08-01T11:00:00', '2025-08-01T11:00:00', false),
+('Lemoine', 'Claire', 'claire.lemoine@example.com', '$2y$10$eqjReaDGnKGfJbGHGib7te.mCZoaLDmcby8GYw1jlIpVoLZykM4kq', '2025-08-01T12:00:00', '2025-08-01T12:00:00', true);
