98fcaa0f074e1a1549c002a4701d88c512f84cc3
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-08-19 23:02:49 +0200

    feat: add NoteController for managing notes with create and retrieve functionalities

---
+++ src/main/java/com/mindvault/backend/controller/NoteController.java
@@ -0,0 +1,54 @@
+package com.mindvault.backend.controller;
+
+import com.mindvault.backend.dto.NoteDTO.NoteDTO;
+import com.mindvault.backend.dto.NoteDTO.NoteResponseDTO;
+import com.mindvault.backend.model.Note;
+import com.mindvault.backend.model.User;
+import com.mindvault.backend.repository.UserRepository;
+import com.mindvault.backend.service.NoteService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/notes")
+public class NoteController {
+    private final NoteService noteService;
+    private final UserRepository userRepository;
+
+    public NoteController(final NoteService noteService, final UserRepository userRepository) {
+        this.noteService = noteService;
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<NoteResponseDTO> createNote(@AuthenticationPrincipal String email, @RequestBody NoteDTO noteDTO) {
+        User user = userRepository.findByEmail(email).orElseThrow();
+        Note note = noteService.createNote(user, noteDTO);
+        return ResponseEntity.ok(toResponseDTO(note));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<NoteResponseDTO>> getUserNotes(@AuthenticationPrincipal String email) {
+        User user = userRepository.findByEmail(email).orElseThrow();
+        List<NoteResponseDTO> notes = noteService.getUserNotes(user)
+                .stream()
+                .map(this::toResponseDTO)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(notes);
+    }
+
+    private NoteResponseDTO toResponseDTO(Note note) {
+        NoteResponseDTO noteResponseDTO = new NoteResponseDTO();
+        noteResponseDTO.setId(note.getId());
+        noteResponseDTO.setTitle(note.getTitle());
+        noteResponseDTO.setContent(note.getContent());
+        noteResponseDTO.setCreationDate(note.getCreationDate());
+        noteResponseDTO.setModificationDate(note.getModificationDate());
+        return noteResponseDTO;
+    }
+}
