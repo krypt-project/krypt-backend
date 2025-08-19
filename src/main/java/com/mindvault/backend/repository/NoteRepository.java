178e6e00ce55f18b850249db3bf6042f4ec9cfd8
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-08-19 22:50:29 +0200

    fix: change primary key type from Integer to Long in NoteRepository interface

--- src/main/java/com/mindvault/backend/repository/NoteRepository.java
+++ src/main/java/com/mindvault/backend/repository/NoteRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface NoteRepository extends JpaRepository<Note, Integer> {
+public interface NoteRepository extends JpaRepository<Note, Long> {
     List<Note> findByUser(User user);
 }
