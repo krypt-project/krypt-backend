76e07d5436172eb99fc3572d4cac6ffe0e5f60af
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-08-19 23:13:45 +0200

    fix: remove redundant getter and setter methods in NoteDTO for cleaner code

--- src/main/java/com/mindvault/backend/dto/NoteDTO/NoteDTO.java
+++ src/main/java/com/mindvault/backend/dto/NoteDTO/NoteDTO.java
@@ -6,4 +6,21 @@ import lombok.Data;
 public class NoteDTO {
     private String title;
     private String content;
+
+    // Getter & Setter
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
