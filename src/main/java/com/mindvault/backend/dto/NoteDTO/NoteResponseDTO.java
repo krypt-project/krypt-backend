e567369b118ce10bc93db5bd0def3d326a09c078
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-08-19 23:13:53 +0200

    fix: remove redundant getter and setter methods in NoteResponseDTO for cleaner code

--- src/main/java/com/mindvault/backend/dto/NoteDTO/NoteResponseDTO.java
+++ src/main/java/com/mindvault/backend/dto/NoteDTO/NoteResponseDTO.java
@@ -12,4 +12,45 @@ public class NoteResponseDTO {
     private String content;
     private LocalDateTime creationDate;
     private LocalDateTime modificationDate;
+
+    // Getter & Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(LocalDateTime modificationDate) {
+        this.modificationDate = modificationDate;
+    }
 }
