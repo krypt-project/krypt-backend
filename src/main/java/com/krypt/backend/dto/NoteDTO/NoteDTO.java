41190f75235db56efa37d84a965ca716dbc559ca
Miche1-Pierre <P.Michel@nancyclotep.com>
2025-09-24 12:03:47 +0200

    refactor: update NoteDTO to add modificationDate to data package

--- src/main/java/com/krypt/backend/dto/NoteDTO/NoteDTO.java
+++ src/main/java/com/krypt/backend/dto/NoteDTO/NoteDTO.java
@@ -2,10 +2,14 @@ package com.krypt.backend.dto.NoteDTO;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class NoteDTO {
     private String title;
     private String content;
+    private LocalDateTime creationDate;
+    private LocalDateTime modificationDate;
 
     // Getter & Setter
     public String getTitle() {
@@ -22,5 +26,21 @@ public class NoteDTO {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(LocalDateTime modificationDate) {
+        this.modificationDate = modificationDate;
     }
 }
