c0a46f69b426a4f4672eed04a39d03c1df5c60e1
Miche1-Pierre <P.Michel@nancyclotep.com>
2025-09-24 11:24:26 +0200

    refactor: update UserController

--- src/main/java/com/krypt/backend/controller/UserController.java
+++ src/main/java/com/krypt/backend/controller/UserController.java
@@ -2,19 +2,12 @@ package com.krypt.backend.controller;
 
 import com.krypt.backend.config.JwtUtils;
 import com.krypt.backend.dto.UserDTO.UserDTO;
-import com.krypt.backend.model.User;
-import com.krypt.backend.repository.UserRepository;
 import com.krypt.backend.service.UserService;
-import io.jsonwebtoken.Jwt;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
