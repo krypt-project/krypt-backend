cba901283d8e73862f3feebd1bbc2c8662af5141
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-10-11 18:59:10 +0200

    fix: remove Lombok (Setter & Getter) doesn't work well again (to do)

--- src/main/java/com/krypt/backend/dto/UserDTO/PatchUserDTO.java
+++ src/main/java/com/krypt/backend/dto/UserDTO/PatchUserDTO.java
@@ -1,18 +1,27 @@
 package com.krypt.backend.dto.UserDTO;
 
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
 
 public class PatchUserDTO {
-    @Getter
-    @Setter
     @Size(max = 50, message = "Last name must be less than 50 characters")
     private String lastName;
 
-    @Getter
-    @Setter
     @Size(max = 50, message = "First name must be less than 50 characters")
     private String firstName;
 
+    public @Size(max = 50, message = "Last name must be less than 50 characters") String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(@Size(max = 50, message = "Last name must be less than 50 characters") String lastName) {
+        this.lastName = lastName;
+    }
+
+    public @Size(max = 50, message = "First name must be less than 50 characters") String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(@Size(max = 50, message = "First name must be less than 50 characters") String firstName) {
+        this.firstName = firstName;
+    }
 }
